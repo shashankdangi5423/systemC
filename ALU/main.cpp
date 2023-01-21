de102065838b862d45739acf1d3b8bf69b413fba
shashankd <shashankd@vayavya.com>
2023-01-21 15:04:05 +0530

    SystemC module for simple alu design

--- ALU/main.cpp
+++ ALU/main.cpp
@@ -1,57 +1,45 @@
 #include<systemc.h>
-#include "ALU.h"
-#include "driver.h"
-#include "monitor.h"
+#include "alu.h"
 
-int sc_main(int argc, char* argv[]){
-	sc_signal< sc_uint<3> > s_OPCODE;
-	sc_signal< sc_uint<4> > s_OP1, s_OP2;
-	sc_signal<bool> s_CARRY, s_ZERO;
-	
-	sc_signal< sc_uint<4> > s_RESULT;
-	
-	ALU alu("ALU");
-	driver dr("stimulus");
-	monitor mon("monitor");
-	
-	//inputs
-	dr.d_OPCODE(s_OPCODE);
-	alu.OPCODE(s_OPCODE);
-	mon.m_OPCODE(s_OPCODE);
-	
-	dr.d_OP1(s_OP1);
-	dr.d_OP2(s_OP2);
-	
-	alu.OP1(s_OP1);
-	alu.OP2(s_OP2);
-	
-	mon.m_OP1(s_OP1);
-	mon.m_OP2(s_OP2);
-	
-	//outputs
-	alu.CARRY(s_CARRY);
-	alu.ZERO(s_ZERO);
-	alu.RESULT(s_RESULT);
-	
-	mon.m_CARRY(s_CARRY);
-	mon.m_ZERO(s_ZERO);
-	mon.m_RESULT(s_RESULT);
-	
-	//tracing
-	sc_trace_file *fp;
-	fp=sc_create_vcd_trace_file("vcd_trace");
-	fp->set_time_unit(1, SC_NS);
-	
-	sc_trace(fp, s_OPCODE, "OPCODE");
-	sc_trace(fp, s_OP1, "OPERAND1");
-	sc_trace(fp, s_OP2, "OPERAND2");
-	sc_trace(fp, s_RESULT, "RESULT");
-	sc_trace(fp, s_CARRY, "CARRY");
-	sc_trace(fp, s_ZERO, "ZERO");
-	
-	//simulation start
-	sc_start(50, SC_NS);
-	sc_close_vcd_trace_file(fp);
-	
-	return 0;
+
+int sc_main(int ac,char *av[]){
+
+    sc_signal<sc_bv<3>> aluctrl;
+    sc_signal <sc_bv<DWORD>> a;
+    sc_signal <sc_bv<DWORD>> b;
+    sc_signal<sc_bv<DWORD>> result;
+    sc_signal< sc_bv<5> > flag;
+
+
+    ALU alu("ALU");
+
+    //connecting alu inputs and outputs to signals
+    alu.aluctrl(aluctrl);
+    alu.a(a);
+    alu.b(b);
+    alu.result(result);
+    alu.flag(flag);
+
+
+    //TEST CASE FOR AND
+    a=10;
+    b=20;
+    aluctrl =  ALU_AND ;
+
+    sc_start(1,SC_NS);
+
+    cout << result<<endl;
+    cout<<flag<<endl; 
+
+    //TEST CASE FOR ADD
+    a=20;
+    b=2;
+    aluctrl =  ALU_ADD ;
+
+    sc_start(1,SC_NS);
+    
+    cout << result<<endl;
+    cout<<flag<<endl;    
+
+    return 0;
 }
---
+++ ALU/mp.h
@@ -0,0 +1,18 @@
+#ifndef MP_H_INCLUDED
+#define MP_H_INCLUDED
+
+#include "systemc.h"
+
+#define DWORD           8     	// Dataword size
+
+
+
+// ALU operations
+#define ALU_AND         0
+#define ALU_OR          1
+#define ALU_ADD         2
+#define ALU_SUB         3
+#define ALU_XOR         4
+					
+
+#endif
